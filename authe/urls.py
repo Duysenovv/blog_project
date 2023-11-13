42db36f6a5a643cdb3285ea8b7cd8b51bd10fafe
Amanzhol_urj <MY_NAME@example.com>
2023-11-13 18:17:55 +0300

    'project_#6'

---
+++ authe/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+
+app_name = 'authe'
+
+urlpatterns = [
+    path('signin/', views.Login.as_view(), name='login'),
+    path('signup/', views.Register.as_view(), name='signup'),
+    path('profile/', views.Profile.as_view(), name='profile'),
+]
